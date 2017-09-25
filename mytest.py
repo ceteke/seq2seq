44fa05be576da82064995ace91cbca19c8655987
Cem Eteke <cemeteke@gmail.com>
2017-09-25 13:19:33 +0300

    rename test

---
+++ mytest.py
@@ -0,0 +1,9 @@
+from seq2seq import Seq2Seq
+import tensorflow as tf
+
+sess = tf.Session()
+# python3 -u  train_s2s.py -e 100 -i 100 -u 1024 -n 1 -em 300 -l 0.01 -d 0.5 -g 5.0 -b 32 -o output/ -s 2000 > log.txt &
+s2s = Seq2Seq(sess=sess,hidden_units=1024, vocab_size=50003, num_layers=1, embedding_size=300, mode='inference', max_decode_len=20)
+s2s.restore('/Users/cem/Desktop/output/seq2seq')
+
+
